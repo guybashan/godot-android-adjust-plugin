16693ca35766a67a575759da2783cd02033d67ae
Guy Bashan <guy.bashan@gmail.com>
2020-07-12 23:15:16 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# godot-android-adjust-plugin
-Android Adjust plugin for Godot Game Engine 3.2 or higher
---
+++ adjust-plugin/src/main/java/com/bashan/godot/adjust/GodotAdjust.java
@@ -0,0 +1,93 @@
+package com.bashan.godot.adjust;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.adjust.sdk.Adjust;
+import com.adjust.sdk.AdjustConfig;
+import com.adjust.sdk.LogLevel;
+
+import org.godotengine.godot.Godot;
+import org.godotengine.godot.plugin.GodotPlugin;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GodotAdjust extends GodotPlugin {
+
+    private static final String TAG = "godot-adjust";
+
+    private Godot activity;
+
+    public GodotAdjust(Godot godot) {
+        super(godot);
+        activity = godot;
+    }
+
+    public void init(final String appToken) {
+        Log.i(TAG, "Started initializing Godot-Adjust Singleton with App Token: " + appToken);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String environment = AdjustConfig.ENVIRONMENT_SANDBOX;
+
+                AdjustConfig config = new AdjustConfig(activity, appToken, environment);
+                config.setLogLevel(LogLevel.VERBOSE);
+
+                Adjust.onCreate(config);
+
+                activity.getApplication().registerActivityLifecycleCallbacks(new AdjustLifecycleCallbacks());
+
+                Log.i(TAG, "Finished initializing Godot-Adjust Singleton");
+            }
+        });
+    }
+
+    private static final class AdjustLifecycleCallbacks implements Application.ActivityLifecycleCallbacks {
+        @Override
+        public void onActivityResumed(Activity activity) {
+            Adjust.onResume();
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+            Adjust.onPause();
+        }
+
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+        }
+    }
+
+    @NonNull
+    @Override
+    public String getPluginName() {
+        return "Godot-Adjust";
+    }
+
+    @NonNull
+    @Override
+    public List<String> getPluginMethods() {
+        return Collections.singletonList("init");
+    }
+}
